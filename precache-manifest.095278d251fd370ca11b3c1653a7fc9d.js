d77aa2ac66d0255ab06ce9c8e0bac68f907d7a12
Pramod <pramodtejrapiku01@gmail.com>
2023-06-05 17:30:25 +0530

    Updates

---
+++ precache-manifest.095278d251fd370ca11b3c1653a7fc9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cbe95fff9702327d99b12bbbabbf77",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "fd9ec5853aca03b551ef",
+    "url": "/bmi-calculator/static/css/main.ebb81fb6.chunk.css"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/js/2.0e5f39ad.chunk.js"
+  },
+  {
+    "revision": "fd9ec5853aca03b551ef",
+    "url": "/bmi-calculator/static/js/main.18262cf4.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
